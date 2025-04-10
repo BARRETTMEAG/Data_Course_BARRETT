04c876664fc2940dca36b3be08fc14a15acba494
BARRETTMEAG <10504850@uvu.edu>
2025-04-09 20:17:58 -0600

    Exam_3

--- Exams/Exam_3/My_Exam_3.R
+++ Exams/Exam_3/My_Exam_3.R
@@ -10,7 +10,7 @@ library(broom)
 library(purrr)
 
 
-# Load the data
+# 1. Load and clean FacultySalaries_1995.csv file and Re-create the graph below ####
 faculty_data <- read.csv("FacultySalaries_1995.csv")
 
 # Create the Rank column based on salary averages
@@ -51,13 +51,17 @@ faculty_data_clean_filtered %>%
                                  "Full" = "Full")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+
+# 2. Build an ANOVA model and display the summary output in your report.####
+
 # Fit the ANOVA model
 anova_model <- aov(Salary ~ State + Tier + Rank, data = faculty_data_clean_filtered)
 
 # Display the summary of the ANOVA model
 summary(anova_model)
 
-# 3. The rest of the test uses another data set. The “Juniper_Oils.csv” data. Get it loaded and take a look. Then tidy it! (show the code used for tidying in your report)
+# 3. The rest of the test uses another data set. The “Juniper_Oils.csv” data. ####
+# Get it loaded and take a look. Then tidy it! (show the code used for tidying in your report)####
 
 juniper_data <- read.csv("Juniper_Oils.csv")
 
@@ -82,7 +86,7 @@ tidy_data <- juniper_data %>%
 # Check the result
 View(juniper_tidy)
 
-# Create the plot
+# 4. Make me a graph of the following: ####
 tidy_data %>% 
   ggplot(aes(x = YearsSinceBurn, y = Concentration)) +
   geom_smooth(aes(group = ChemicalID)) +
@@ -90,8 +94,22 @@ tidy_data %>%
   scale_x_continuous(limits = c(0, 20), breaks = seq(5, 20, by = 5)) + 
   theme_minimal() +
   labs(x = "YearsSinceBurn", 
-       y = "Concentration")
-  
+       y = "Concentration")+
+  scale_y_continuous(breaks = function(x) {  # Define custom breaks for specific chemicals
+    if (any(grepl("compound", unique(tidy_data$ChemicalID)))) {
+      return(seq(0, 40, by = 10)) 
+    }  elif {
+      return(seq(0, 30, by = 10))
+    } elif {
+      return(seq(0, 20, by = 5))   
+    } elif {
+      return(seq(0, 10, by = 2.5))
+    } 
+  })
+
+
+# 5. Use a generalized linear model to find which chemicals show ####
+# concentrations that are significantly (significant, as in P < 0.05) affected by “Years Since Burn”.###
 
 # Step 1: Run GLM for each chemical separately
 lm_results <- tidy_data %>%
